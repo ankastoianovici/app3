6ca7cd8c242c032ae11613c0d5dfdcad54d26ba5
Anka-Snezana Stoianovici <anka.stoianovici@gmail.com>
2023-01-14 21:39:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/app/Citire.java
@@ -0,0 +1,54 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Citire extends AppCompatActivity {
+    BottomNavigationView nav;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+        nav=findViewById(R.id.nav);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ServerDetails:
+                        replaceFragment(new ServerDetails());
+                        break;
+                    case R.id.Browse:
+                        replaceFragment(new Browse());
+                        break;
+                    case R.id.DataAccess:
+                        replaceFragment(new DataAccess());
+                        break;
+                    case R.id.AlarmsEvents:
+                        replaceFragment(new AlarmsEvents());
+                        break;
+                    case R.id.RuntimeDevelopment:
+                        replaceFragment(new RuntimeDevelopment());
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.app;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.prosysopc.ua.SessionActivationException;
+import com.prosysopc.ua.UserIdentity;
+import com.prosysopc.ua.client.UaClient;
+
+import org.opcfoundation.ua.transport.security.SecurityMode;
+
+import java.net.URISyntaxException;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText serverUriTextField;
+    private String serverUri;
+    public static UaClient myClient;
+    private String error;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button connectBtn= findViewById(R.id.connectBtn);
+        serverUriTextField = findViewById(R.id.serverTf);
+
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                serverUri = serverUriTextField.getText().toString();
+
+                try {
+                    myClient = createClient(serverUri);
+                    myClient.connect();
+                    Intent intent = new Intent(getApplicationContext(), Citire.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    error = e.toString();
+                    showAlert("Error", "Failed to connect");
+                }
+            }
+        });
+    }
+    private void showAlert(String title, String message){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.show();
+    }
+    private UaClient createClient(String serverUri) throws URISyntaxException, SessionActivationException {
+        UaClient myClient = new UaClient(serverUri);
+        myClient.setLocale(Locale.ENGLISH);
+        myClient.setTimeout(60000);
+        myClient.setSecurityMode(SecurityMode.NONE);
+        myClient.setUserIdentity(new UserIdentity());
+        return myClient;
+    }
+    public static UaClient getMyClient(){
+        return myClient;
+    }
+}
