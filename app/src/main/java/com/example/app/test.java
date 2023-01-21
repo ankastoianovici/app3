ce88d4bb05219b97c6d81c5f8b4c7b74a8f56e7d
Anka-Snezana Stoianovici <anka.stoianovici@gmail.com>
2023-01-21 21:59:39 +0200

    21.59

--- app/src/main/java/com/example/app/test.java
+++ app/src/main/java/com/example/app/test.java
@@ -9,22 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class test extends AppCompatActivity {
 
-    private LinearLayout btn_hide_show,btn_hide_show1,layout_hide_show,layout_hide_show1;
-
+    private LinearLayout btn_hide_show,layout_hide_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
         btn_hide_show=findViewById(R.id.btn_cash);
         layout_hide_show = findViewById(R.id.layout_hide_show);
-        btn_hide_show1=findViewById(R.id.btn);
-        layout_hide_show1 = findViewById(R.id.layout_hide_show1);
-
-        btn_hide_show.setOnClickListener(new View.OnClickListener() {
+             btn_hide_show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int invisible=layout_hide_show.getVisibility();
-                int invisible1=layout_hide_show1.getVisibility();
                 if(invisible==View.VISIBLE){
                     layout_hide_show.setVisibility(View.GONE);
                 }
@@ -33,17 +28,6 @@ public class test extends AppCompatActivity {
                 }
             }
         });
-        btn_hide_show1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int invisible1=layout_hide_show1.getVisibility();
-                if(invisible1==View.VISIBLE){
-                    layout_hide_show1.setVisibility(View.GONE);
-                }
-                else {
-                    layout_hide_show1.setVisibility(View.VISIBLE);
-                }
-            }
-        });
+
     }
 }
